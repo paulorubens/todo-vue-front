0056c4b014ce2032a7d40fa686d710d3b1ec2b41
Paulo Rubens <paulorubenslima@gmail.com>
2021-12-05 01:53:43 -0300

    Adicionando bootstrap e font-awesome; Criando layout da página principal

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:16.13.0-alpine3.14
 
 RUN apk add --no-cache bash
 
-RUN npm i -g @vue/cli
+# RUN npm i -g @vue/cli
 
 USER node
 
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>ToDo List <span class="fa fa-check" /></h1>
+    </div>
+    <div id="main">
+      <div class="container">
+        <h5>Tarefas
+          <span class="badge bg-info">
+            {{ tarefas.filter(tarefa => tarefa.completed === false).length }}
+          </span>
+        </h5>
+        <div class="card">
+          <div class="card-body">
+            <input class="form-control" type="text" placeholder="Informe a próxima tarefa">
+            <br />
+            <ul class="list-group">
+              <li class="list-group-item"
+                v-for="tarefa in tarefas" v-bind:key="tarefa"
+                v-bind:class="{ 'text-decoration-line-through': tarefa.completed }">
+                {{ tarefa.description }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      tarefas: [
+      {
+        'description': 'tarefa 2',
+        'completed': false
+      },
+      {
+        'description': 'tarefa 3',
+        'completed': false
+      },
+      {
+        'description': 'tarefa 1',
+        'completed': true
+      },
+    ]
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #F1F1F1;
+  padding-top: 30px;
+  padding-bottom: 30px;
 }
 </style>
